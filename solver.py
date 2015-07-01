93e9bdd61d7ad0d1f7bc368c311cab0d2e8c7127
Joseph Utecht <thenmal@gmail.com>
2015-07-01 13:48:05 -0500

    brute force working

--- solver.py
+++ solver.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from colorama import Fore, Back, Style
 from colorama import init
 from grid import Grid, Orb
+import copy
+import cProfile
 
 init()
 
@@ -50,3 +52,65 @@ hg.print_grid()
 
 gs.full_move()
 hg.full_move()
+
+def create_initial_grid():
+    grid = []
+    for x in range(6):
+        for y in range(5):
+            grid.append([(x,y)])
+    return grid
+
+def all_possible_moves(move):
+    last_move = move[-1]
+    new_moves = []
+    # up
+    if last_move[1] > 0:
+        new_moves.append(move + [(last_move[0], last_move[1] - 1)])
+    # down
+    if last_move[1] < 4:
+        new_moves.append(move + [(last_move[0], last_move[1] + 1)])
+    # left
+    if last_move[0] > 0:
+        new_moves.append(move + [(last_move[0] - 1, last_move[1])])
+    # right
+    if last_move[0] < 5:
+        new_moves.append(move + [(last_move[0] + 1, last_move[1])])
+    return new_moves
+
+def brute_generate(depth):
+    moves = create_initial_grid()
+    for i in range(depth):
+        new_level = []
+        for move in moves:
+            new_level.extend(all_possible_moves(move))
+        if i == 0:
+            moves = new_level
+        else:
+            moves.extend(new_level)
+    return moves
+
+def solve(grid, move):
+    for i, m in enumerate(move):
+        if i > 0:
+            grid.swap(move[i - 1], m)
+    return grid.full_move()
+
+def brute_force(depth):
+    best_combo = -1
+    best_orb = -1
+    best_move = None
+    for move in brute_generate(depth):
+        combo, orbs = solve(copy.deepcopy(g), move)
+        if combo > best_combo:
+            best_move = move
+            best_combo = combo
+            best_orb = orbs
+        elif combo == best_combo and orbs > best_orb:
+            best_move = move
+            best_combo = combo
+            best_orb = orbs
+
+    print("{} combo, {} orbs".format(best_combo, best_orb))
+    print(best_move)
+
+cProfile.run("brute_force(5)")
